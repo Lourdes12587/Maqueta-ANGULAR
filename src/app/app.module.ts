b4e977d7c9bbb57e108c8c7484720993ba98368e
Lourdes <luliortiz12802@gmail.com>
2023-03-15 23:51:06 -0300

    denuevo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './componente/header/header.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { LoginComponent } from './login/login.component';
-import { RedesComponent } from './redes/redes.component';
-import { AcercadeComponent } from './acercade/acercade.component';
-import { EducacionComponent } from './educacion/educacion.component';
-import { ExperienciaComponent } from './experiencia/experiencia.component';
-import { PortfolioComponent } from './portfolio/portfolio.component';
-import { ContactoComponent } from './contacto/contacto.component';
+import { NavbarComponent } from './COMPONENTES/navbar/navbar.component';
+import { LoginComponent } from './COMPONENTES/login/login.component';
+import { RedesComponent } from './COMPONENTES/redes/redes.component';
+import { AcercadeComponent } from './COMPONENTES/acercade/acercade.component';
+import { EducacionComponent } from './COMPONENTES/educacion/educacion.component';
+import { ExperienciaComponent } from './COMPONENTES/experiencia/experiencia.component';
+import { PortfolioComponent } from './COMPONENTES/portfolio/portfolio.component';
+import { ContactoComponent } from './COMPONENTES/contacto/contacto.component';
+import { HeaderComponent } from './COMPONENTES/header/header.component';
 
 
 @NgModule({
     declarations: [
     AppComponent,
-    HeaderComponent,
     NavbarComponent,
     LoginComponent,
     RedesComponent,
@@ -24,6 +23,7 @@ import { ContactoComponent } from './contacto/contacto.component';
     ExperienciaComponent,
     PortfolioComponent,
     ContactoComponent,
+    HeaderComponent,
 
   ],
   imports: [
